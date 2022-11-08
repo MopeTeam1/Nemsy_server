44b452ed19ecab486dedac531817d319a3546c8e
SuperH0ng <81635179+SuperH0ng@users.noreply.github.com>
2022-11-09 05:00:46 +0900

    feat : like-

--- src/main/java/com/nemsy/nemsy_server/controller/like_law/LikeLawController.java
+++ src/main/java/com/nemsy/nemsy_server/controller/like_law/LikeLawController.java
@@ -2,10 +2,7 @@ package com.nemsy.nemsy_server.controller.like_law;
 
 import com.nemsy.nemsy_server.service.like_law.LikeLawService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +13,10 @@ public class LikeLawController {
     @PostMapping("/bill/${billId}/likes")
     public int likeLaw(@PathVariable String billId, @RequestParam String userId) {
         return likeLawService.likeLaw(billId, userId);
+    }
+
+    @DeleteMapping("/bill/${billId}/likes")
+    public int unlikeLaw(@PathVariable String billId, @RequestParam String userId) {
+        return likeLawService.unlikeLaw(billId, userId);
     }
 }
--- src/main/java/com/nemsy/nemsy_server/domain/law/LawRepository.java
+++ src/main/java/com/nemsy/nemsy_server/domain/law/LawRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LawRepository extends JpaRepository<Law, String> {
 
+
 }
